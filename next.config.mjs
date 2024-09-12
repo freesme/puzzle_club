86faf634ff66a08a358ef0ea6dd3c1c55957f7b9
Krane <freesme@163.com>
2024-09-12 11:17:32 +0800

    ignore eslint in build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // 在构建时忽略 ESLint 错误
+    },
+};
 
 export default nextConfig;
